8a3693d2cb27727df1cda4536ddca9b65fb6697d
Lucas Montenegro <144347120+lukinhas563@users.noreply.github.com>
2024-03-19 10:56:00 -0300

    Update UpdateById.ts

--- src/server/controllers/colaborators/UpdateById.ts
+++ src/server/controllers/colaborators/UpdateById.ts
@@ -29,5 +29,9 @@ export const updateById = async (
     console.log(req.params);
     console.log(req.body);
 
+    if (Object.keys(req.body).length === 0) {
+        return res.status(400).json({ error: 'O corpo da requisição não pode estar vazio.' });
+    }
+
     return res.status(500).json({ result: 'UPDATE A COLABORATOR BY ID NOT IMPLEMENTED' });
 };
